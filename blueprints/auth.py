bbf86dfae5e5054bcc751f4d422d640a188138ce
Nat0110tan <tan00250@umn.edu>
2023-02-19 02:06:59 -0600

    added mail functions

--- blueprints/auth.py
+++ blueprints/auth.py
@@ -1,4 +1,6 @@
 from flask import Blueprint, render_template
+from exts import mail
+from flask_mail import Message
 
 bp = Blueprint("auth", __name__, url_prefix="/auth")
 
@@ -9,3 +11,10 @@ bp = Blueprint("auth", __name__, url_prefix="/auth")
 @bp.route("/login")
 def register():
   return render_template("login.html")
+
+@bp.route("/mail/test")
+def mail_test():
+  msg = Message('Hello', sender = "internetexplorers2023@gmail.com", recipients = ['2843182910@qq.com'])
+  msg.body = "InternetExplorers跟你打招呼说： hey 李睿婷是傻子🐷"
+  mail.send(msg)
+  return "Sent"
--- config.py
+++ config.py
@@ -1,3 +1,9 @@
 
 DATABASE_URL01="postgresql://project_iy1t_user:cGeIVmZGk9nQHRhLtvOGJQRGFr0PbtU9@dpg-cfntom9mbjsmoccs0rug-a.oregon-postgres.render.com/project_iy1t"
 SQLALCHEMY_DATABASE_URI = DATABASE_URL01
+MAIL_SERVER = "smtp.gmail.com"
+MAIL_PORT = 465
+MAIL_USERNAME = "internetexplorers2023@gmail.com"
+MAIL_PASSWORD = "wydkbuzydmvjjjss"
+MAIL_USE_TLS = False
+MAIL_USE_SSL = True
